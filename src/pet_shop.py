1830bfacf045546d41dd2e63d2b081ed297e0f79
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-10-21 15:24:55 +0100

    Completed homeword

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,69 @@
-# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop,cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, amount):
+    pet_shop["admin"]["pets_sold"] += amount
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pet_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pet_breed.append(pet)
+    
+    return pet_breed
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            pet_shop["pets"].remove(pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, cash):
+    customer["cash"] -= cash
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    else:
+        return False
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet:
+        pet_price = pet["price"]
+    else:
+        return
+    
+    if customer_can_afford_pet(customer, pet):
+        add_pet_to_customer(customer, pet)
+        remove_pet_by_name(pet_shop, pet)
+        add_or_remove_cash(pet_shop, pet_price)
+        remove_customer_cash(customer, pet_price)
+        increase_pets_sold(pet_shop, 1)
